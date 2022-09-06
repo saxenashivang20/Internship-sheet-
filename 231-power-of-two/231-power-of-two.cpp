c6882faed24f8dd510cf347b450a47dbeb4f0d11
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-09-06 15:42:46 +0530

    Time: 3 ms (60.07%), Space: 5.9 MB (27.39%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n==0) return false;
-        while(n%2==0) n/=2;
-        return n==1;  }
+        long x =(long) n;;
+        return (x &(x-1))==0;  }
 };
