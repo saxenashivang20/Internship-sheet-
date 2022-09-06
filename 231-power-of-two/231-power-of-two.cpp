244558794b231c409ff19e499c7457d61c2f7871
Shivang Saxena <saxenashivang20@gmail.com>
2022-09-06 18:21:10 +0530

    Update 231-power-of-two.cpp

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -5,3 +5,11 @@ public:
         long x =(long) n;
         return (x &(x-1))==0; }
 };
+
+/*class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        while(n%2==0) n/=2;
+        return n==1;  }
+};*/
