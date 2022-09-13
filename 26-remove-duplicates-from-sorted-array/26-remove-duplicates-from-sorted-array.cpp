77e7e8daf0b57198c35c7f525288639df84f33a4
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-09-13 22:37:06 +0530

    Time: 28 ms (25.04%), Space: 18.2 MB (93.01%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,8 +4,7 @@ public:
         int j =1;
         for(int i = 1;i<nums.size();i++){
             if(nums[i]!=nums[i-1]){
-            nums[j]=nums[i];
-            j++;
+            nums[j++]=nums[i];
             }
         }
         return j;
