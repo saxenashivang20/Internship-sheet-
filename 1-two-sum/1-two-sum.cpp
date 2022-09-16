214cb461241f6f1df46580f01efbf2cc09a69004
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-09-17 00:05:15 +0530

    Time: 907 ms (11.42%), Space: 10.1 MB (89.02%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
+                    ans.emplace_back(i);
+                    ans.emplace_back(j);
 					break;
                 }
             }
