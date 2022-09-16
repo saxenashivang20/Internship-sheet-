1b2b86ea872fbd181ca952bea6d4c50702dcbd57
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-09-17 00:05:42 +0530

    Time: 905 ms (11.49%), Space: 10.2 MB (89.02%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    ans.emplace_back(i);
-                    ans.emplace_back(j);
-					break;
+         int a=0;
+        int b=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    a=i;
+                    b=j;
                 }
             }
         }
-        return ans;
+        return {a,b};
     }
 };
