79d7073655e153425a2b3ac894971637ebbf19b8
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-09-25 23:58:36 +0530

    Time: 3 ms (62.36%), Space: 6.4 MB (99.17%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int ans = 0;
-        int n = s.size();
-        for(int i = n-1;i>=0;--i){
+        for(int i = s.size()-1;i>=0;--i){
             if(s[i]== ' ' && ans > 0)return ans;
             if(s[i]!= ' ') ans++;
         }
