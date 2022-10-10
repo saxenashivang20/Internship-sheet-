6d2458180788e107b56297df79bc38e74d077cb0
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-10-10 22:05:57 +0530

    Time: 162 ms (58.13%), Space: 46.7 MB (80.58%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,10 +3,9 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n;i++)
             if(nums[i]==nums[i-1])
             return true;
-        }
         return false;
     }
 };
