b0439ad06f4162d26271cdf688e551021b985ccb
Shivang Saxena <saxenashivang20@gmail.com>
2022-10-19 23:58:51 +0530

    Update 0896-monotonic-array.cpp

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isMonotonic(vector<int>& nums) {
+    bool isMonotonic(vector<int>& nums) { //array is said to be monotonic in nature if it is either continuously increasing or continuously decreasing.
         bool increase = true;
         bool decrease = true;
         for(int i = 0; i < nums.size () - 1; i++) {
