f08ac7758408d70917778b351eae2706a7e01868
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2022-11-22 20:24:17 +0530

    Time: 157 ms (45.26%), Space: 33.6 MB (94.08%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -9,7 +9,7 @@ public:
         }
         for(int i =0; i<nums.size();++i)
             if(nums[i] > 0)
-                ans.emplace_back(i+1);
+                ans.push_back(i+1);
         return ans;
     }
 };
