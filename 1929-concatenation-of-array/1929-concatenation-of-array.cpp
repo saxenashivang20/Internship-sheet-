9003555ad2090f9bdf63c236d4536ec2fb8a6262
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2023-01-15 15:43:46 +0530

    Time: 12 ms (62.46%), Space: 12.8 MB (55.18%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
         for(int i =0;i<n;i++){
-            nums.push_back(nums[i]);
+            nums.emplace_back(nums[i]);
         }
         return nums;
     }
