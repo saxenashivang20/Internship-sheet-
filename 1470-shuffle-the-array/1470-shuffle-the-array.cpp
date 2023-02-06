0dc118cd29dbaf6f6e4e3a3b247fe727fe133846
Shivang Saxena <shivang.saxena_cs20@gla.ac.in>
2023-02-06 23:36:06 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (31.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector <int> ans;
         for(int i=0; i<n; i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[i+n]);
+            ans.emplace_back(nums[i]);
+            ans.emplace_back(nums[i+n]);
         }
         return ans;
     }
